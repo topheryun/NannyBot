5146c075f1ddb616d8c8427a54746fbfd76f1d2a
topheryun <topheryun@gmail.com>
2022-01-17 15:50:45 -0800

    added documentation

--- functions/birthday-display.js
+++ functions/birthday-display.js
@@ -1,3 +1,12 @@
+/**
+ * takes the birthday values from birthdays.json and returns them 
+ * in YYYY-MM-DD format, because that's the only format that makes
+ * sense.
+ * @param day Number
+ * @param month Three character String
+ * @param year Number
+ * @return String
+ */
 function displayBirthday(day,month,year) {
   return `${year}-${month}-${day}`;
 }
