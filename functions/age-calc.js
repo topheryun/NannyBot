39a18c89a50d822663170c4bbdea1a740615be1a
topheryun <topheryun@gmail.com>
2022-01-17 16:56:52 -0800

    age calc working

--- functions/age-calc.js
+++ functions/age-calc.js
@@ -1,3 +1,10 @@
+/**
+ * Input a birthday to get how old they are.
+ * @param {*} day number 1-31
+ * @param {*} month Three character String
+ * @param {*} year 4 digit number
+ * @returns object holding year, month, day
+ */
 function calculateAge(day,month,year) {
   const monthNumber = convertMonthToNumber(month);
   const birthday = new Date(year, monthNumber, day);
@@ -21,6 +28,12 @@ function calculateAge(day,month,year) {
   return result;
 }
 
+/**
+ * Input a month number based on Node.js' Date().
+ * Will return how many days are in that month.
+ * @param monthNumber the number is based on the month - 1
+ * @returns number of days in that month
+ */
 function numberOfDaysInMonth(monthNumber) {
   // 28 days: Feb
   if (monthNumber == 1) return 28;
@@ -30,6 +43,12 @@ function numberOfDaysInMonth(monthNumber) {
   else return 31;
 }
 
+/**
+ * Converts a month in three character String format to a 
+ * number that is based on the month minus one.
+ * @param month Three character String representing a month
+ * @returns number based on month - 1
+ */
 function convertMonthToNumber(month) {
  switch (month) {
    case "Jan": return 0;
