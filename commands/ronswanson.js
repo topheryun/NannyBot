7048906bf81df46ad2f4d814a3c41bbf76bbf230
topheryun <topheryun@gmail.com>
2022-01-19 17:06:21 -0800

    ron swanson done in 1 minute.

---
+++ commands/ronswanson.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require("node-fetch");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('ronswanson')
+	.setDescription('Ron Swanson.'),
+	async execute(interaction) {
+    console.log(`${interaction.user.username} summoned Ron Swanson.`);
+    const response = await fetch("http://ron-swanson-quotes.herokuapp.com/v2/quotes");
+    if (response.status == 200) {
+      const data = await response.json();
+      await interaction.reply({content: `${data}`, ephemeral: true});
+    }	
+	}
+};
