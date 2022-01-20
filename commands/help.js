c48c63ce3520e5b209fdaddd794035f30267d3cd
topheryun <topheryun@gmail.com>
2022-01-19 16:56:49 -0800

    help.js just lists all commands

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Just gives you all commands available.')
+		.setDefaultPermission(false),
+	async execute(interaction) {
+    console.log(`${interaction.user.username} asked for help.`);
+		await interaction.reply({
+      content: `age\nbirthday\njoke\nweather`,
+      ephemeral: true
+    });
+	},
+};
--- todo.md
+++ todo.md
@@ -7,5 +7,6 @@
 - [Poems](https://poems.one/api/poem/)
 - [Game Deals](https://apidocs.cheapshark.com/?version=latest#intro)
 - [MTG](https://docs.magicthegathering.io/) Requires SDK; https calls don't work with spaces
+- [Scryfall](https://scryfall.com/docs/api/cards/named)
 - [Ron Swanson](https://github.com/jamesseanwright/ron-swanson-quotes)
 - [Music Recommendations](https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0)
