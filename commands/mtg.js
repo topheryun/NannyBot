a90dac9df562fde0fff28826a90b3a069c437357
topheryun <topheryun@gmail.com>
2022-01-19 19:55:29 -0800

    expanded mtg functionality

--- commands/mtg.js
+++ commands/mtg.js
@@ -10,27 +10,41 @@ module.exports = {
       .setDescription('The input to echo back')
       .setRequired(true)),
 	async execute(interaction) {
-    let fuzzyAppend = interaction.options.getString('input');
-    console.log(`${interaction.user.username} searched for a MTG card with the string: ${fuzzyAppend}`);
-    let autocompleteAppend = fuzzyAppend;
-    if (fuzzyAppend.includes(' ')) {
-      fuzzyAppend = fuzzyAppend.replaceAll(' ', '%20');
-    }
-    if (autocompleteAppend.includes(' ')) {
-      autocompleteAppend = autocompleteAppend.substring(0, autocompleteAppend.indexOf(' '));
-    }
-    let response = await fetch("https://api.scryfall.com/cards/named?fuzzy=" + fuzzyAppend);
-    if (response.status == 200) {
-      const data = await response.json();
-      await interaction.reply({files: [data.image_uris.normal], ephemeral: true});
-    }
-    else if (response.status == 404) {
-      response = await fetch("https://api.scryfall.com/cards/autocomplete?q=" + autocompleteAppend);
+    if (interaction.options.getString('input') === 'random') {
+      console.log(`${interaction.user.username} wants to see a random MTG card.`);
+      const response = await fetch("https://api.scryfall.com/cards/random");
       if (response.status == 200) {
         const data = await response.json();
-        const botResponse = JSON.stringify(data.data);
-        await interaction.reply({content: `Try one of these: ` + botResponse, ephemeral: true})
+        await interaction.reply({files: [data.image_uris.normal], ephemeral: true});
       }
     }
+    else {
+      let fuzzyAppend = interaction.options.getString('input');
+      console.log(`${interaction.user.username} searched for a MTG card with the string: ${fuzzyAppend}`);
+      let autocompleteAppend = fuzzyAppend;
+      if (fuzzyAppend.includes(' ')) {
+        fuzzyAppend = fuzzyAppend.replaceAll(' ', '%20');
+      }
+      if (autocompleteAppend.includes(' ')) {
+        autocompleteAppend = autocompleteAppend.substring(0, autocompleteAppend.indexOf(' '));
+      }
+      let response = await fetch("https://api.scryfall.com/cards/named?fuzzy=" + fuzzyAppend);
+      if (response.status == 200) {
+        const data = await response.json();
+        await interaction.reply({files: [data.image_uris.normal], ephemeral: true});
+      }
+      else if (response.status == 404) {
+        response = await fetch("https://api.scryfall.com/cards/autocomplete?q=" + autocompleteAppend);
+        if (response.status == 200) {
+          const data = await response.json();
+          if (data.data.length == 0) {
+            await interaction.reply({content: `Seriously? Try searching better.`, ephemeral: true});
+          } else {
+            const botResponse = JSON.stringify(data.data);
+            await interaction.reply({content: `Try one of these: ` + botResponse, ephemeral: true})
+          }
+        }
+      }
+    } 
 	},
 };
--- todo.md
+++ todo.md
@@ -3,6 +3,8 @@
 - [Anime](https://jikan.moe/)
 - [Poems](https://poems.one/api/poem/)
 - [Game Deals](https://apidocs.cheapshark.com/?version=latest#intro)
-- [MTG](https://docs.magicthegathering.io/) Requires SDK; https calls don't work with spaces
-- [Scryfall](https://scryfall.com/docs/api/cards/named)
 - [Music Recommendations](https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0)
+
+## fix birthday and age
+- needs to be more modular. put functions in different files. 
+- create /jojo which will give her birthday and age?
