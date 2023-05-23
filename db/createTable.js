1929b0f994e7bdf405d25a1891abf156ff154019
topheryun <topheryun@gmail.com>
2023-05-23 11:30:22 -0500

    match data saved in db

--- db/createTable.js
+++ db/createTable.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE chris (matchId VARCHAR(255) PRIMARY KEY, championName VARCHAR(255), win BOOL, kills INT(255), deaths INT(255), assists INT(255), kda DOUBLE(255,2), totalDamageDealtToChampions INT(255), damagePerMinute DOUBLE(255,2))";
+  var sql = "CREATE TABLE eunjung (matchId VARCHAR(255) PRIMARY KEY, championName VARCHAR(255), win BOOL, kills INT(255), deaths INT(255), assists INT(255), kda DOUBLE(255,2), totalDamageDealtToChampions INT(255), damagePerMinute DOUBLE(255,2))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
